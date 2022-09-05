751f18656402652e7e1081811c0a43532f6ab9d9
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-09-05 10:23:21 +0100

    initialise backend

---
+++ src/main/java/com/example/mythologybackend/MythologyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mythologybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythologyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythologyBackendApplication.class, args);
+	}
+
+}
